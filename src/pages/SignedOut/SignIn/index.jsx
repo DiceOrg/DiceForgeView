787e4b6c11706c692c09bd82a399a3d9aae6572f
sebHanssen <sebastian.hanssen@outlook.com>
2024-03-15 11:54:41 +0100

    create SignIn.jsx layout

--- src/pages/SignedOut/SignIn/index.jsx
+++ src/pages/SignedOut/SignIn/index.jsx
@@ -1,12 +1,19 @@
 export default function SignIn() {
 
     return (
-        <>
-        <img src=""></img>
-        <p>
-            
-            SignIn Work
-        </p>
-        </>
+        <div className="sign-in-container">
+            <img src="https://res.cloudinary.com/dhvcqoipp/image/upload/v1710498763/logo_xevjg4.png"></img>
+            <div className="input-area">
+                <input type="text" required />
+                <div className="labelline">Username</div>
+            </div>
+            <div className="input-area">
+                <input type="password" required />
+                <div className="labelline">Password</div>
+            </div>
+            <div className="register-label">
+                No account? Click here to register.
+            </div>
+        </div>
     );
-  }
+}
