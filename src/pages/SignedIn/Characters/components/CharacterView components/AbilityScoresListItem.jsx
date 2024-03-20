b8b38f789f811bdb91a63147de25a910b03b47bc
Sebank <sebank@gmail.com>
2024-03-20 14:34:07 +0100

    messing around with ability

--- src/pages/SignedIn/Characters/components/CharacterView components/AbilityScoresListItem.jsx
+++ src/pages/SignedIn/Characters/components/CharacterView components/AbilityScoresListItem.jsx
@@ -1,26 +1,61 @@
-export default function AbilityScoresListItem({ ability_name, ability, setCharacter, character }) {
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
-    if (ability_name == "id")
-        return <></>
+export default function AbilityScoresListItem({ ability_name, ability_ref, setCharacter, character }) {
 
     let prof_value = 2;
+
+    const [ability, setAbility] = useState(ability_ref);
+
+    async function updateAbility() {
+        try {
+            console.log("started")
+          const jwtToken = Cookies.get('jwt');
+    
+          const response = await fetch(`https://localhost:7256/character/Ability/${ability.id}`, {
+            method: 'PUT',
+            headers: {
+              'Authorization': `Bearer ${jwtToken}`,
+              'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(ability),
+          });
+          console.log("finished", response);
+    
+          if (response.ok) {
+            const data = await response.json();
+            console.log('Data, abliity:', data, ability);
+          } else {
+            console.error('Failed to fetch data:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error.message);
+        }
+      }
+
+    useEffect(() => {
+        updateAbility();
+    }, [ability])
 
     const change = (event) => {
         const { name, value } = event.target;
         let objectToChange = { ...character };
-        if (name == "prof")
+        if (name == "prof"){
             objectToChange.abilities[ability_name].prof ^= true;
-        else if (name == "value" && !isNaN(value)){
+        }else if (name == "value" && !isNaN(value)){
             objectToChange.abilities[ability_name].value = value;
-            if ( value > 30)
+            if ( value > 30){
                 objectToChange.abilities[ability_name].value = 30;
+            }else if (value < 0){
+                objectToChange.abilities[ability_name].value = 0;
+            }
         }
         else
             return;
         setCharacter(objectToChange);
+        setAbility(objectToChange.abilities[ability_name]);
+        console.log("change", objectToChange.abilities[ability_name])
     }
-
-    
 
     return (
         <div className="row no-wrap">
