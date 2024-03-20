fe74b68d445cad0ec920579ebba6ccc8b436bb0e
SBjer1 <sebastian.engan@no.experis.com>
2024-03-20 13:08:10 +0100

    component refactoriung

--- src/pages/SignedIn/Characters/components/CharacterView.jsx
+++ src/pages/SignedIn/Characters/components/CharacterView.jsx
@@ -1,5 +1,6 @@
 import Skills from "../../Skills";
 import Abilities from "../Abilities/Index";
+import CharacterHeader from "./CharacterHeader";
 
 export default function CharacterView({ character, setCharacter }) {
   if (character == null) return <>loading...</>;
@@ -14,42 +15,7 @@ export default function CharacterView({ character, setCharacter }) {
           </div>
         </div>
         <div className="column">
-          <div className="character-header">
-            <div className="hit-points">
-              <div className="hp">
-                <button>+</button>
-                <button>-</button>
-                <h2>50</h2>
-                <p>HP</p>
-              </div>
-              <div>
-                <p>36</p>
-                <p>Current</p>
-              </div>
-              <div>
-                <p>50</p>
-                <p>Max</p>
-              </div>
-              <div>
-                <p>0</p>
-                <p>temp</p>
-              </div>
-            </div>
-            <div className="Character-header-right">
-              <div>
-                <p>16</p>
-                <p>AC</p>
-              </div>
-              <div>
-                <p>3</p>
-                <p>Initiativ</p>
-              </div>
-              <div>
-                <p>45</p>
-                <p>Speed</p>
-              </div>
-            </div>
-          </div>
+            <CharacterHeader/>
         </div>
       </div>
       <div className="row">
