497240a8c780175107bd9cf20f2af67804a82abe
Sebastian <sebastian.hanssen@outlook.com>
2024-03-20 13:56:44 +0100

    update css

--- src/pages/SignedOut/SignIn/index.jsx
+++ src/pages/SignedOut/SignIn/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { LoginContext } from "../../../App";
 import Cookies from "js-cookie";
+import "../signedout.css";
 
 
 export default function SignIn() {
