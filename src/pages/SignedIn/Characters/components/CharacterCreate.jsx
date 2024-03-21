8a0b912753512eae1acc80c6b6ff76ec5adbda04
Sebank <sebank@gmail.com>
2024-03-21 14:49:02 +0100

    started created

--- src/pages/SignedIn/Characters/components/CharacterCreate.jsx
+++ src/pages/SignedIn/Characters/components/CharacterCreate.jsx
@@ -1,10 +1,13 @@
+import { useEffect } from "react";
+
 export default function CharacterCreate() {
+
 
     return (
         <div className="characters-container">
             <h1>Create New Character</h1>
             <span>
-                Name: <input type="text"></input>
+                Name: <input type="text" name="name" ></input>
             </span>
             <button>Create</button>
         </div>
