4fdc2097656fb5af1e12cba0d36681827101d2e3
Sebank <sebank@gmail.com>
2024-03-21 15:14:42 +0100

    need a merge from Hanssen

--- src/pages/SignedIn/Characters/components/CharacterCreate.jsx
+++ src/pages/SignedIn/Characters/components/CharacterCreate.jsx
@@ -1,15 +1,44 @@
-import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import Cookies from "js-cookie";
 
 export default function CharacterCreate() {
 
+    const [name, setName] = useState("");
+    const navigate = useNavigate();
+
+    async function createCharacter() {
+        try {
+            console.log(name);
+            const jwtToken = Cookies.get('jwt');
+
+            await fetch(`https://localhost:7256/character/?name=${name}`, {
+                method: 'PUT',
+                headers: {
+                    'accept': "*/*",
+                    'Authorization': `Bearer ${jwtToken}`,
+                }
+            }
+            ).then(res => res.json());
+            
+            navigate("/Characters");
+
+        } catch (error) {
+            console.error('Error fetching data:', error.message);
+        }
+    }
+
+    const changeName = (event) => {
+        setName(event.target.value);
+    }
 
     return (
         <div className="characters-container">
             <h1>Create New Character</h1>
             <span>
-                Name: <input type="text" name="name" ></input>
+                Name: <input type="text" onChange={(event) => changeName(event)}></input>
             </span>
-            <button>Create</button>
+            <button onClick={() => createCharacter()}>Create</button>
         </div>
     );
   }
